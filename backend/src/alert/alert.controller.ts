4901ca4d22e3accfd9dcdaff917f9132ea630181
SanamShivakoti23 <snshiwakoti28@gmail.com>
2023-05-31 19:56:08 +0545

    Changed alertname variable to alertName i.e. Camel Case, as well as chanaged createnewalert to create and also formatted the document with the help of prettier extension. lastly, Add part of update code to give update functionality.

--- backend/src/alert/alert.controller.ts
+++ backend/src/alert/alert.controller.ts
@@ -1,30 +1,59 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response } from 'express';
 
-import Alert from "./alert.model";
-import { createAlert } from "./alert.service";
+import Alert from './alert.model';
+import { createAlert } from './alert.service';
+import { errorMessages } from '../common/config/messages';
 
-const createnewalert = async(req: Request, res:Response, next:NextFunction)=>{
+const create = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { symbol, title, price, target, alertName, notes, expiresAt } =
+      req.body;
 
-    try{
-        const {symbol, title, price, target, alertname, notes, expiresAt} = req.body;
-        
-        const alert = await createAlert(
-            symbol,
-            title,
-            price,
-            target,
-            alertname,
-            notes,
-            expiresAt ,
-        );
+    const alert = await createAlert(
+      symbol,
+      title,
+      price,
+      target,
+      alertName,
+      notes,
+      expiresAt
+    );
 
-        res.status(201);
-        res.json(alert);
-
-    }catch(error){
-        next(error);
-    }
-
+    res.status(201);
+    res.json(alert);
+  } catch (error) {
+    next(error);
+  }
 };
 
-export{createnewalert};
+const update = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { symbol, title, price, alertName, notes, expiresAt } = req.body;
+
+    const update = await Alert.findById(req.params.id);
+    if (!update) {
+      throw new Error(errorMessages.OBJECT_WITH_ID_NOT_FOUND);
+    }
+
+    update.symbol = symbol;
+    update.title = title;
+    update.price = price;
+    update.alertName = alertName;
+    update.notes = notes;
+    update.expiresAt = expiresAt;
+    await update.save();
+
+    res.status(200).json(update);
+  } catch (error: any) {
+    if (error.message === errorMessages.OBJECT_WITH_ID_NOT_FOUND) {
+      return res.status(404).json({ message: error.message });
+    }
+    next(error);
+  }
+};
+
+export { create, update };
