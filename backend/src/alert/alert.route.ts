59e44c12f6ca9095922f1c42e0493733f5f3234a
SanamShivakoti23 <snshiwakoti28@gmail.com>
2023-05-31 20:36:06 +0545

    Changed createnewalert to create as convection as well as add route for update.

--- backend/src/alert/alert.route.ts
+++ backend/src/alert/alert.route.ts
@@ -3,8 +3,9 @@ const router = express.Router();
 import { validateRequest } from '../common/middlewares/validateRequest';
 
 import { createalertSchema } from './alert.schema';
-import { createnewalert } from './alert.controller';
+import { create, update } from './alert.controller';
+import { updateOrderSchema } from '../orders/orders.schema';
 
-router.post('/', createalertSchema, validateRequest, createnewalert);
-
+router.post('/', createalertSchema, validateRequest, create);
+router.patch('/update/:id', updateOrderSchema, validateRequest, update);
 export default router;
