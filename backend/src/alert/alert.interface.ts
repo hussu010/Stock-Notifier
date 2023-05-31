0a65783a1c1fb3257bfb3b497539660326b8292c
SanamShivakoti23 <snshiwakoti28@gmail.com>
2023-05-31 19:59:58 +0545

    Changed Ialert to IAlert i.e. pascal case, also changed alertname to alertName i.e. camel case.

--- backend/src/alert/alert.interface.ts
+++ backend/src/alert/alert.interface.ts
@@ -1,12 +1,12 @@
-interface Ialert extends Document{
-    _id: string;
-    symbol: string;
-    title: string;
-    price: number;
-    target:number;
-    alertname: string;
-    notes: string;
-    expiresAt: number;
+interface IAlert extends Document {
+  _id: string;
+  symbol: string;
+  title: string;
+  price: number;
+  target: number;
+  alertName: string;
+  notes: string;
+  expiresAt: number;
 }
 
-export { Ialert };
+export { IAlert };
