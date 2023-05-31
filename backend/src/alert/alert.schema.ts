72f8e484d1245d9e033a16bb7629664ad57deed3
SanamShivakoti23 <snshiwakoti28@gmail.com>
2023-05-31 20:40:05 +0545

    Add Schema for alert update, and also formatted the document with the help of prettier extension.

--- backend/src/alert/alert.schema.ts
+++ backend/src/alert/alert.schema.ts
@@ -32,7 +32,7 @@ const createalertSchema = [
       }),
       body('expiresAt')
       .custom((value,{req})=>{
-          if(value <=0 || value > 32){
+          if(value <=0 || value > 32){ 
             throw new Error('The expire time should be between 1 to 32 days');
           }
         
@@ -40,5 +40,45 @@ const createalertSchema = [
       }),
 
 ];
+
+const updatealertSchema = [
+  body('symbol').custom((value, { req }) => {
+      const { id, name } = binarySearchStockArray(stockSymbolData, value);
+      if (id === 0) {
+        throw new Error('Invalid stock symbol');
+      }
+  
+      req.body.title = name;
+  
+      return true;
+    }),
+    body('price')
+    .isNumeric()
+    .custom((value, {req})=>{
+      if(value > req.body.target ){
+          throw new Error('The price should be greater than 0 and less than or equal to target value');
+      }
+      return true;
+    }),
+    body('target')
+    .isNumeric()
+    .custom((value,{req})=>{
+       if(value < req.body.price){
+          throw new Error('The target value should be greater than price.');
+       }
+       return true;
+    }),
+    body('expiresAt')
+    .custom((value,{req})=>{
+        if(value <=0 || value > 32){ 
+          throw new Error('The expire time should be between 1 to 32 days');
+        }
+      
+      return true;
+    }),
+
+];
+
+
 
 export {createalertSchema};
