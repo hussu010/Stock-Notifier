55aba58be480492f2c351082f8a9406f7caf1f60
SanamShivakoti23 <snshiwakoti28@gmail.com>
2023-05-31 20:33:13 +0545

    Changed Ialert to IAlert and also formatted the documnent with the help of prettier extension.

--- backend/src/alert/alert.model.ts
+++ backend/src/alert/alert.model.ts
@@ -1,22 +1,20 @@
 import mongoose from 'mongoose';
-import { Ialert } from './alert.interface';
+import { IAlert } from './alert.interface';
 
-const alertSchema = new mongoose.Schema<Ialert>(
-
-    {
-        symbol: {type: String, required: true},
-        title: {type: String, required:true},
-        price: {type: Number, required: true},
-        target: {type: Number, required: true},
-        alertname: {type: String, required: true},
-        notes: {type: String, required: true},
-        expiresAt: {type: Number, required: true},
-    },
-    {
-        timestamps: true,
-    }
-
+const alertSchema = new mongoose.Schema<IAlert>(
+  {
+    symbol: { type: String, required: true },
+    title: { type: String, required: true },
+    price: { type: Number, required: true },
+    target: { type: Number, required: true },
+    alertName: { type: String, required: true },
+    notes: { type: String, required: true },
+    expiresAt: { type: Number, required: true },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-const Alert = mongoose.model<Ialert>('alert', alertSchema);
+const Alert = mongoose.model<IAlert>('alert', alertSchema);
 export default Alert;
