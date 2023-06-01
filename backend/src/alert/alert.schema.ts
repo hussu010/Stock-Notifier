7bfc734cf7fa21add7a7e0fa41c4d7ab31f9eaf3
SanamShivakoti23 <snshiwakoti28@gmail.com>
2023-06-01 10:52:58 +0545

    Changed 32 days upper limit to 365 days

--- backend/src/alert/alert.schema.ts
+++ backend/src/alert/alert.schema.ts
@@ -1,84 +1,82 @@
-import mongoose from "mongoose";
-import {body, param} from 'express-validator';
-import { binarySearchStockArray } from "../common/utils/stock";
-import { stockSymbolData } from "../common/config/constants";
+import mongoose from 'mongoose';
+import { body, param } from 'express-validator';
+import { binarySearchStockArray } from '../common/utils/stock';
+import { stockSymbolData } from '../common/config/constants';
 
 const createalertSchema = [
-    body('symbol').custom((value, { req }) => {
-        const { id, name } = binarySearchStockArray(stockSymbolData, value);
-        if (id === 0) {
-          throw new Error('Invalid stock symbol');
-        }
-    
-        req.body.title = name;
-    
-        return true;
-      }),
-      body('price')
-      .isNumeric()
-      .custom((value, {req})=>{
-        if(value > req.body.target ){
-            throw new Error('The price should be greater than 0 and less than or equal to target value');
-        }
-        return true;
-      }),
-      body('target')
-      .isNumeric()
-      .custom((value,{req})=>{
-         if(value < req.body.price){
-            throw new Error('The target value should be greater than price.');
-         }
-         return true;
-      }),
-      body('expiresAt')
-      .custom((value,{req})=>{
-          if(value <=0 || value > 32){ 
-            throw new Error('The expire time should be between 1 to 32 days');
-          }
-        
-        return true;
-      }),
+  body('symbol').custom((value, { req }) => {
+    const { id, name } = binarySearchStockArray(stockSymbolData, value);
+    if (id === 0) {
+      throw new Error('Invalid stock symbol');
+    }
 
+    req.body.title = name;
+
+    return true;
+  }),
+  body('price')
+    .isNumeric()
+    .custom((value, { req }) => {
+      if (value > req.body.target) {
+        throw new Error(
+          'The price should be greater than 0 and less than or equal to target value'
+        );
+      }
+      return true;
+    }),
+  body('target')
+    .isNumeric()
+    .custom((value, { req }) => {
+      if (value < req.body.price) {
+        throw new Error('The target value should be greater than price.');
+      }
+      return true;
+    }),
+  body('expiresAt').custom((value, { req }) => {
+    if (value <= 0 || value > 32) {
+      throw new Error('The expire time should be between 1 to 32 days');
+    }
+
+    return true;
+  }),
 ];
 
 const updatealertSchema = [
   body('symbol').custom((value, { req }) => {
-      const { id, name } = binarySearchStockArray(stockSymbolData, value);
-      if (id === 0) {
-        throw new Error('Invalid stock symbol');
-      }
-  
-      req.body.title = name;
-  
-      return true;
-    }),
-    body('price')
-    .isNumeric()
-    .custom((value, {req})=>{
-      if(value > req.body.target ){
-          throw new Error('The price should be greater than 0 and less than or equal to target value');
-      }
-      return true;
-    }),
-    body('target')
-    .isNumeric()
-    .custom((value,{req})=>{
-       if(value < req.body.price){
-          throw new Error('The target value should be greater than price.');
-       }
-       return true;
-    }),
-    body('expiresAt')
-    .custom((value,{req})=>{
-        if(value <=0 || value > 32){ 
-          throw new Error('The expire time should be between 1 to 32 days');
-        }
-      
-      return true;
-    }),
+    const { id, name } = binarySearchStockArray(stockSymbolData, value);
+    if (id === 0) {
+      throw new Error('Invalid stock symbol');
+    }
 
+    req.body.title = name;
+
+    return true;
+  }),
+  body('price')
+    .isNumeric()
+    .custom((value, { req }) => {
+      if (value > req.body.target) {
+        throw new Error(
+          'The price should be greater than 0 and less than or equal to target value'
+        );
+      }
+      return true;
+    }),
+  body('target')
+    .isNumeric()
+    .custom((value, { req }) => {
+      if (value < req.body.price) {
+        throw new Error('The target value should be greater than price.');
+      }
+      return true;
+    }),
+  body('expiresAt').custom((value, { req }) => {
+    if (value <= 0 || value > 365) {
+      throw new Error('The expire time should be between 1 to 365 days');
+    }
+
+    return true;
+  }),
 ];
 
-
-
-export {createalertSchema};
+export { createalertSchema };
