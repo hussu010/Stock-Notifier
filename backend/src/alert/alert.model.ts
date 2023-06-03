edfa53e8475ddae8cc51c4840ff7082a0ced296a
SanamShivakoti23 <snshiwakoti28@gmail.com>
2023-06-03 13:35:17 +0545

    changed the type of expiesAt i.e. Date and added code for default value for expire date of 1 year from today date. Also adde new field i.e. type.

--- backend/src/alert/alert.model.ts
+++ backend/src/alert/alert.model.ts
@@ -1,16 +1,20 @@
 import mongoose from 'mongoose';
 import { IAlert } from './alert.interface';
 
+//default value for expire date of 1 year from today date
+const aYearFromNow = new Date();
+aYearFromNow.setFullYear(aYearFromNow.getFullYear() + 1);
+
 const alertSchema = new mongoose.Schema<IAlert>(
   {
     symbol: { type: String, required: true },
     title: { type: String, required: true },
     price: { type: Number, required: true },
     target: { type: Number, required: true },
+    type: {type: Number, required: true},
     alertName: { type: String, required: true },
     notes: { type: String, required: true },
-    expiresAt: { type: Number, required: true },
-    
+    expiresAt: { type: Date, required: true, default: aYearFromNow },
   },
   {
     timestamps: true,
