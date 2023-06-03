948b2453e95c002afe9473cd961f92464627deae
SanamShivakoti23 <snshiwakoti28@gmail.com>
2023-06-03 13:37:19 +0545

    Changed the route of update and delete alert from /update/:id to /:id and /delete/:id to /:id

--- backend/src/alert/alert.route.ts
+++ backend/src/alert/alert.route.ts
@@ -8,6 +8,6 @@ import { getAll, create, update,remove } from './alert.controller';
 
 router.get('/', getAll);
 router.post('/', createalertSchema, validateRequest, create);
-router.patch('/update/:id', updatealertSchema, validateRequest, update);
-router.delete('/delete/:id', deletealertSchema, validateRequest, remove);
+router.patch('/:id', updatealertSchema, validateRequest, update);
+router.delete('/:id', deletealertSchema, validateRequest, remove);
 export default router;
