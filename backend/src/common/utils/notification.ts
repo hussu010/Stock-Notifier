7795b2cd61e372e8414531901cdf2e13b91c8547
SanamShivakoti23 <snshiwakoti28@gmail.com>
2023-06-09 15:33:07 +0545

    Added NotificationTriggered.

--- backend/src/common/utils/notification.ts
+++ backend/src/common/utils/notification.ts
@@ -1,6 +1,8 @@
 import Order from '../../orders/orders.model';
+import Alert from '../../alerts/alerts.model';
 import { getStockPrice } from './stock';
 import { sendMessageToDiscord } from './discord';
+import { error } from 'console';
 
 const scanNotificationTriggers = async () => {
   try {
@@ -49,4 +51,76 @@ const checkNotificationTriggered = (
   return { notificationTriggered: false, notificationType: '' };
 };
 
-export { scanNotificationTriggers };
+
+
+//For sending the notification of user created notes as alert
+const NotificationTriggers = async () => {
+  try {
+    const alerts = await Alert.find();
+    for (let i = 0; i < alerts.length; i++) {
+      var alert = alerts[i];
+      const currentStockPrice = await getStockPrice(alert.symbol);
+      const alerType = alert.type;
+      console.log(alerType);
+      // if the user select the GREATER_THAN option, when the price will cross the target and becomes greater than the system will use this message
+      if (alerType === 'GREATER_THAN') {
+        const { notificationTriggered, notificationType } =
+          checkNotificationGreater(currentStockPrice, alert.target);
+
+        if (notificationTriggered) {
+          const notificationMessage = `
+          Symbol:  ${alert.symbol}\n
+          Title: ${alert.title}\n
+          Current Price: Rs. ${currentStockPrice}\n
+          Target: Rs. ${alert.target}\n
+          Notes: ${alert.notes}\n
+          Expire At: ${alert.expiresAt}`;
+
+          sendMessageToDiscord(
+            `${notificationType} alert for ${alert.symbol}`,
+            notificationMessage
+          );
+        }
+      }
+      // if the user select the LESSER_THAN option, when the price will below the target and becomes lesser than the system will use this message
+      else if (alerType === 'LESS_THAN') {
+        const { notificationTriggered, notificationType } =
+          checkNotificationLesser(currentStockPrice, alert.target);
+
+        if (notificationTriggered) {
+          const notificationMessage = `
+        Symbol:  ${alert.symbol}\n
+        Title: ${alert.title}\n
+        Current Price: Rs. ${currentStockPrice}\n
+        Target: Rs. ${alert.target}\n
+        Notes: ${alert.notes}\n
+        Expire At: ${alert.expiresAt}`;
+
+          sendMessageToDiscord(
+            `${notificationType} alert for ${alert.symbol}`,
+            notificationMessage
+          );
+        }
+      }
+    }
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+};
+
+//if the target price will become greater than current price
+const checkNotificationGreater = (current: number, target: number) => {
+  if (current > target) {
+    return { notificationTriggered: true, notificationType: 'INCREASED TARGET PRICE' };
+  } 
+  return { notificationTriggered: false, notificationType: '' };
+};
+
+//if the target price will below than current price
+const checkNotificationLesser = (current: number, target: number) => {
+  if (current < target) {
+    return { notificationTriggered: true, notificationType: 'DECREASED TARGET PRICE' };
+  } 
+  return { notificationTriggered: false, notificationType: '' };
+};
+export { scanNotificationTriggers, NotificationTriggers };
