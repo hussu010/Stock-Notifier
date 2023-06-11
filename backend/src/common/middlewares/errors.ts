c30855b1debf912afccfc93a47b45af95b886570
SanamShivakoti23 <snshiwakoti28@gmail.com>
2023-06-11 14:44:47 +0545

    Added next() in errorResponder code block.

--- backend/src/common/middlewares/errors.ts
+++ backend/src/common/middlewares/errors.ts
@@ -8,12 +8,17 @@ const errorLogger: ErrorRequestHandler = (err, req, res, next) => {
       req.method
     } - ${req.ip}`
   );
+  // Continue to the next middleware or route handler
   next(err);
 };
 
 const errorResponder: ErrorRequestHandler = (err, req, res, next) => {
   const status = err.status || 500;
+  // Send the error response
   res.status(status).json({ message: err.message });
+
+  // Continue to the next middleware or router handler
+  next();
 };
 
 export { errorLogger, errorResponder };
