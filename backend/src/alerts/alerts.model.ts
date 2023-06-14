ef1861da4f24ccf226096112b45e89c80ad7ffaf
SanamShivakoti23 <snshiwakoti28@gmail.com>
2023-06-14 12:20:19 +0545

    Added AlertTypeEnum and AlertStatusEnum for enum in alert model.

--- backend/src/alerts/alerts.model.ts
+++ backend/src/alerts/alerts.model.ts
@@ -1,15 +1,16 @@
 import mongoose from 'mongoose';
 import { IAlert } from './alerts.interface';
+import { AlertTypeEnum, AlertStatusEnum } from './alerts.interface';
 
 const alertSchema = new mongoose.Schema<IAlert>(
   {
     symbol: { type: String, required: true },
     title: { type: String, required: true },
     target: { type: Number, required: true },
-    type: { type: String, required: true },
+    type: { type: String, required: true, enum: AlertTypeEnum },
     notes: { type: String },
     expiresAt: { type: Date },
-    status: { type: String, default: 'OPEN' },
+    status: { type: String, default: 'OPEN', enum: AlertStatusEnum },
   },
   {
     timestamps: true,
