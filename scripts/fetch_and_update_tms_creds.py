f0b7bd241fa9b4941ece9fc0caac81135eb227c9
hussu <hussu010@gmail.com>
2023-09-29 12:34:52 +0545

    use tms api for authentication and remove selemium

--- scripts/fetch_and_update_tms_creds.py
+++ scripts/fetch_and_update_tms_creds.py
@@ -1,60 +1,95 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 import requests
 from dotenv import load_dotenv
 import os
+import base64
+import re
 
 load_dotenv()
 
-driver = webdriver.Firefox()
-
-TMS_URL = "https://tms32.nepsetms.com.np/login"
+# TMS_URL = "https://tms32.nepsetms.com.np"
+TMS_URL = "https://demotrading.nepalstock.com"
 USERNAME = os.getenv("TMS_USERNAME")
 PASSWORD = os.getenv("TMS_PASSWORD")
 DETA_API_KEY = os.getenv("DETA_API_KEY")
 
-driver.set_window_size(300, 800)
-driver.get(TMS_URL)
+password_bytes = PASSWORD.encode('utf-8')
+password_base64_encoded = base64.b64encode(password_bytes)
+password_base64_encoded_string = password_base64_encoded.decode('utf-8')
 
-username_field = driver.find_element(By.XPATH, '//input[@placeholder="Client Code/ User Name"]')
-password_field = driver.find_element(By.ID, "password-field")
-captcha_input = driver.find_element(By.ID, "captchaEnter")
-login_button = driver.find_element(By.XPATH, '//input[@type="submit" and @value="Login"]')
+get_captcha_id = requests.get(
+    f"{TMS_URL}/tmsapi/authApi/captcha/id",
+    headers={
+        "Referer": f"{TMS_URL}/login",
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0",
+        "Host": TMS_URL.split("//")[-1],
+    })
+captcha_id = get_captcha_id.json()["id"]
 
-username_field.send_keys(USERNAME)
-password_field.send_keys(PASSWORD)
+get_captcha_image = requests.get(
+    f"{TMS_URL}/tmsapi/authApi/captcha/image/{captcha_id}",
+    stream=True,
+    headers={
+        "Referer": f"{TMS_URL}/login",
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0",
+        "Host": TMS_URL.split("//")[-1],
+    })
+
+get_captcha_image.raise_for_status()
+image_path = 'captcha_image.jpg'
+with open(image_path, 'wb') as file:
+    for chunk in get_captcha_image.iter_content(chunk_size=8192):
+        file.write(chunk)
+os.system(f'open {image_path}')
 
 captcha_value = input("Please enter the captcha value displayed on the page: ")
-captcha_input.send_keys(captcha_value)
 
-login_button.click()
+authenticate_user_response = requests.post(
+    f"{TMS_URL}/tmsapi/authApi/authenticate",
+    json={
+        "userName": USERNAME,
+        "password": password_base64_encoded_string,
+        "captchaIdentifier": captcha_id,
+        "userCaptcha": captcha_value
+    },
+    headers={
+        "Content-Type": "application/json",
+        "Origin": TMS_URL,
+        "Referer": f"{TMS_URL}/login",
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0",
+        "Host": TMS_URL.split("//")[-1],
+    })
 
-all_cookies = driver.get_cookies()
+client_id = authenticate_user_response.json()["data"]["clientDealerMember"]["client"]["id"]
 
-_rid = all_cookies[0]["value"]
-_aid = all_cookies[1]["value"]
-xsrfToken = all_cookies[2]["value"]
+if (authenticate_user_response.status_code == 200):
+    print("User authenticated successfully")
+else:
+    print("oopse, there was an error authenticating the user")
+    print(authenticate_user_response.status_code)
+    exit()
+
+cookies = authenticate_user_response.headers["Set-Cookie"].split(', ')
+rid = re.search('_rid=([^;]*)', cookies[0]).group(1)
+aid = re.search('_aid=([^;]*)', cookies[1]).group(1)
+xsrf_token = re.search('XSRF-TOKEN=([^;]*)', cookies[2]).group(1)
+
+print("Fetched authentication cookies successfully")
 
 headers = {
     "Content-Type": "application/json",
     "X-Space-App-Key": DETA_API_KEY
 }
 data = {
-    "_rid": _rid,
-    "_aid": _aid,
-    "xsrfToken": xsrfToken,
+    "_rid": rid,
+    "_aid": aid,
+    "xsrfToken": xsrf_token,
+    "clientId": f"{client_id}"
 }
 
+print("Writing cookies to deta")
 response = requests.patch("https://notifier-1-b7933411.deta.app/api/tms-auth/", headers=headers, json=data)
 if (response.status_code == 200):
     print("Writing cookies to deta complete")
 else:
     print("oopse, there was an error writing cookies")
     print(response.json())
-
-print("Closing the browser in 5 seconds")
-import time
-time.sleep(5)
-print("Closing the browser now")
-
-driver.quit()
